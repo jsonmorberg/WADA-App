193a2009ba0063b4d179cdde11bb18ec13c2dce0
River <yearianriver@gmail.com>
2021-02-09 12:33:14 -0800

    update to river branch

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,7 @@ import 'dictionary.dart';
 import 'package:image_picker/image_picker.dart';
 import 'profile.dart';
 import 'placeholder_widget.dart';
+import 'water_stats.dart';
 class Home extends StatelessWidget {
   Home({this.uid});
   final String uid;
@@ -144,71 +145,4 @@ class MainPage extends StatelessWidget {
     );
   }
 
-}
-class AddPlant extends StatefulWidget {
-  AddPlant({Key key}) : super(key: key);
-
-  @override
-  _AddPlant createState() => _AddPlant();
-}
-
-class _AddPlant extends State {
-
-  File _image;
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  void open_camera()
-  async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
-    setState(() {
-      _image = image;
-    });
-
-  }
-  void open_gallery()
-  async {
-    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      _image = image;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Add plant profile"),
-          backgroundColor: Colors.black45,),
-        body: Center(
-          child: Container(
-            child: Column(
-              children: [
-                Container(
-                  color: Colors.lightGreen,
-                  height: 200.0,
-                  width: 200.0,
-                  child: _image == null ? Text("Still waiting!") : Image.file(_image),),
-                FlatButton(
-                  color: Colors.deepOrangeAccent,
-                  child: Text("Open Camera", style: TextStyle(color: Colors.white),),
-                  onPressed: (){
-                    open_camera();
-                  },),
-                FlatButton(
-                  color: Colors.limeAccent,
-
-                  child:Text("Open Gallery", style: TextStyle(color: Colors.black),),
-                  onPressed: (){
-                    open_gallery();
-                  },
-                )
-              ],
-            ),
-          ),
-        )
-
-    );
-
-  }
 }
--- lib/water_stats.dart
+++ lib/water_stats.dart
@@ -1,7 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class WaterStats extends StatelessWidget {
   @override
@@ -34,3 +35,74 @@ class WaterStats extends StatelessWidget {
 
 }
 
+
+
+class AddPlant extends StatefulWidget {
+  AddPlant({Key key}) : super(key: key);
+
+  @override
+  _AddPlant createState() => _AddPlant();
+}
+
+class _AddPlant extends State {
+
+  File _image;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void open_camera()
+  async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+
+  }
+  void open_gallery()
+  async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    var user = firebase.auth().currentUser;
+    return Scaffold(
+        appBar: AppBar(title: Text("Add plant profile"),
+          backgroundColor: Colors.black45,),
+        body: Center(
+          child: Container(
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.lightGreen,
+                  height: 200.0,
+                  width: 200.0,
+                  child: _image == null ? Text("Still waiting!") : Image.file(_image),),
+                FlatButton(
+                  color: Colors.deepOrangeAccent,
+                  child: Text("Open Camera", style: TextStyle(color: Colors.white),),
+                  onPressed: (){
+                    open_camera();
+                  },),
+                FlatButton(
+                  color: Colors.limeAccent,
+
+                  child:Text("Open Gallery", style: TextStyle(color: Colors.black),),
+                  onPressed: (){
+                    open_gallery();
+                  },
+                )
+              ],
+            ),
+          ),
+        )
+
+    );
+
+  }
+}
