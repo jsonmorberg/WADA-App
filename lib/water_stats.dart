4cad190c1b8c96c563ee83dc0db7384707111698
cameronCJW <cmrnwllc@gmail.com>
2021-02-17 16:20:40 -0800

    Watering Page Skeleton UI

--- lib/water_stats.dart
+++ lib/water_stats.dart
@@ -3,34 +3,51 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 
-class WaterStats extends StatelessWidget {
+class WaterStats extends StatefulWidget {
+  final String name;
+
+  WaterStats({this.name});
+  @override
+  State<StatefulWidget> createState() => _WaterStatsState();
+}
+
+class _WaterStatsState extends State<WaterStats> {
+  String title = 'Watering';
+  String day = "Today";
+  String room1 = "Bedroom";
+  String room2 = "Living Room";
+
   @override
   Widget build(BuildContext context) {
-    final title = 'Grid List';
 
     return MaterialApp(
       title: title,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
-          crossAxisCount: 2,
-          // Generate 100 widgets that display their index in the List.
-          children: List.generate(100, (index) {
-            return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            );
-          }),
-        ),
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Center(child: _buildColumns(),
+          )
       ),
     );
   }
 
-}
+  Widget _buildRoom() => ListView();
 
+  Widget _buildRoomView() => Column();
+
+  Widget _buildVineColumn() => Column();
+
+  Widget _buildColumns() => Container(
+    decoration: BoxDecoration(
+      color: Colors.black45,
+    ),
+    child: Column(
+      children: [
+        _buildVineColumn(),
+        _buildRoomView(),
+      ],
+    ),
+  );
+
+}
