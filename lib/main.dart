e80ea9c36d7e1db16716cd2f67100690d8461f45
Jayson Morberg <jaysonmorberg9@gmail.com>
2021-02-18 15:20:54 -0800

    added notification package dependency

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'home.dart';
 import 'signup.dart';
